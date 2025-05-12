396b86ee78c67203498af6967ad484800abd53ea
Loveleen C <loveleenchaudhari@gmail.com>
2025-05-12 22:01:21 +0530

    disable square after selection, make sprite appear after clicking

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,24 +1,21 @@
+import { useState } from "react";
 
 const Square = ({id, getSquareId, text}) => {
+    const [isDisabled, setDisabled] = useState(false)
 
-    {/* <img src="src/assets/ivysaur.gif" onClick={handleImgClick}/> */}
+    
     const handleImgClick = (event) => {
         console.log("clicking image")
         event.stopPropagation();
+        event.target.src = "src/assets/ivysaur.gif"
         const parent = event.currentTarget.parentNode;
         getSquareId(parent)
-        event.target.src = "src/assets/ivysaur.gif"
-    }
-
-    const handleClick = (event) => {
-        event.stopPropagation();
-        const button = event.currentTarget.parentNode;
-        getSquareId(parent)
+        setDisabled(true)
     }
 
     return(<>
-        <button id={id} className='block'>
-            <img src="data:," onClick={handleImgClick}/>
+        <button id={id} className='block' disabled={isDisabled}>
+            <img className='playerSprite' src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" onClick={handleImgClick}/>
         </button>
     </>)
 }
